27eb5ddea0a545fc4e280dd95149baac8738004c
jsmin6330 <jsmin6330@gmail.com>
2024-01-09 15:27:30 +0900

    user join form 연결 추가

--- PrivateStay/src/main/java/com/test/controller/UserController.java
+++ PrivateStay/src/main/java/com/test/controller/UserController.java
@@ -1,33 +1,41 @@
 package com.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.test.entity.User;
 import com.test.service.UserService;
 
-@RestController
+
+
+@Controller
 @RequestMapping("/main/user")
 public class UserController {
 	
 	@Autowired
 	private UserService userService;
 	
+	@GetMapping("/join")
+    public String showJoinForm(Model model) {
+        model.addAttribute("user", new User());
+        return "joinUser_form"; // Thymeleaf 템플릿 파일의 이름
+    }
+	
 	@PostMapping("/join") //http://localhost:8080/main/join
-	public ResponseEntity<?> joinUser(@RequestBody User user){
+	public String joinUser(@ModelAttribute User user, Model model){
 		try {
-			User joinUser = userService.joinUser(user);
-			String successMessage = "가입이 완료되었습니다.";
-			return new ResponseEntity<>(successMessage, HttpStatus.CREATED); 
+			userService.joinUser(user);
+			model.addAttribute("message", "가입이 완료되었습니다.");
+			return "forPrivateUser"; // 가입 성공 페이지로 이동
 		} catch (RuntimeException e) {
-			return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+			model.addAttribute("error", e.getMessage());
+			return "joinUser_form"; // 가입 실패 시 회원가입 폼을 다시 표시
 		}
 	}
 	
@@ -41,6 +49,5 @@ public class UserController {
           
             return "아이디 또는 비밀번호가 올바르지 않습니다.";
         }
-		
 	}
 }
