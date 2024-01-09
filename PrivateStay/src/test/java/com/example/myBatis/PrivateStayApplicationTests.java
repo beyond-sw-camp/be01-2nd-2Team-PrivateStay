b189b7abb1704698464a8a0ed21bea56fa062c28
ae-chae <aechae11@gmail.com>
2024-01-09 10:04:02 +0900

    project update

---
+++ PrivateStay/src/test/java/com/example/myBatis/PrivateStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myBatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
