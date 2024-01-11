2682c4331217b9cff7288229b657e683436e928a
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-11 12:59:15 +0900

    2024.1.11. pm12:56

--- PrivateStay/src/main/java/com/test/service/BusinessService.java
+++ PrivateStay/src/main/java/com/test/service/BusinessService.java
@@ -5,8 +5,8 @@ import com.test.entity.Business;
 public interface BusinessService {
 
 	Business saveBusiness(Business business); 	// 1. 사업자 가입(추가)하기
-	Business getBusinessById(String businessId);	// 2. 사업자 찾기
-	Business updateBusiness(String businessId, Business updatedBusiness); 	// 3. 사업자 수정하기
-    void deleteBusiness(String businessId);		// 4. 사업자 삭제하기
-    boolean loginBusiness(String businessId, String business_pwd);  				// 5. 사업자 로그인
+	Business getBusinessByCode(String bId);	// 2. 사업자 찾기
+	Business updateBusiness(String bId, Business updatedBusiness); 	// 3. 사업자 수정하기
+    void deleteBusiness(String bId);		// 4. 사업자 삭제하기
+    boolean loginBusiness(String bId, String bpwd);  				// 5. 사업자 로그인
 }
