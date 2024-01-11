2201bbad7c7c898bccc4c3511d99c918d90fa49d
jun16017108 <150647370+jun16017108@users.noreply.github.com>
2024-01-11 15:50:11 +0900

    Create ReservationForm.java

---
+++ PrivateStay/src/main/java/com/test/controller/ReservationForm.java
@@ -0,0 +1,10 @@
+package jpa.privatestay.domain.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+public class ReservationForm {
+    private String userId;
+    private String sCode;
+}
