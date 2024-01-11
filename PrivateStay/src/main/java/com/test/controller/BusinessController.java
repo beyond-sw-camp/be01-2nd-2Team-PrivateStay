d1f8739000a7a7accc41ca5f3e5dfbf74dff1f9c
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-11 12:56:11 +0900

    2024.1.11. pm12:56

--- PrivateStay/src/main/java/com/test/controller/BusinessController.java
+++ PrivateStay/src/main/java/com/test/controller/BusinessController.java
@@ -1,69 +1,77 @@
 package com.test.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.test.entity.Business;
 import com.test.service.BusinessService;
 
-@RestController
-@RequestMapping("main/business")
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/main/business/menu")
 public class BusinessController {
 	
 	@Autowired
 	private BusinessService businessService;
 	
-	@PostMapping 			// http://localhost:8080/api/business
-	public ResponseEntity<Business> saveBusiness(@RequestBody Business business) {
-	
-		Business saveBusiness = businessService.saveBusiness(business);
+	@GetMapping("/profile") // 사업자 프로필 보기
+    public String businessProfileForm(Model model, HttpSession session) {
+		String bId = (String) session.getAttribute("bId");
 		
-		return new ResponseEntity<>(saveBusiness, HttpStatus.CREATED);
-	}
-	
-	@GetMapping("{id}")		 // http://localhost:8080/api/business/id입력
-	public ResponseEntity<Business> getBusinessById(@PathVariable("id") String businessId) {
-		Business business =  businessService.getBusinessById(businessId);
-		return ResponseEntity.ok(business);
-	}
-	
-	@PutMapping("{id}")		 // http://localhost:8080/api/business/id입력
-    public ResponseEntity<Business> updateBusiness(@PathVariable("id") String businessId,
-                                                   @RequestBody Business updatedBusiness) {
-        Business business = businessService.updateBusiness(businessId, updatedBusiness);
-        if (business != null) {
-            return ResponseEntity.ok(business);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+		if (bId != null) {
+	        Business currentbusiness = businessService.getBusinessByCode(bId);
+	        model.addAttribute("business", currentbusiness);
+	    } else {
+	    	System.out.println("유효하지 않은 접근입니다.");
+	    	return "redirect:/main/business/login"; //로그인 페이지로 이동
+	    }
+		
+        return "BupdateProfile"; // Thymeleaf 템플릿 파일의 이름
     }
+	
+	@PostMapping("/profile") //유저 프로필 보기
+	public String BupdateProfile(@ModelAttribute Business updatedbusiness, HttpSession session, Model model) { 
+		String bId = (String) session.getAttribute("bId");
+		
+		if (updatedbusiness.getBusiness_pwd() == null || updatedbusiness.getBusiness_pwd().isEmpty()) {
+			model.addAttribute("error", "비밀번호는 공백이 될 수 없습니다.");
+			return "BupdateProfile";
+	    }
+		
+		try {
+            updatedbusiness.setBusiness_code(bId); // 세션에서 받은 userId를 설정
+            Business business = businessService.updateBusiness(bId, updatedbusiness);
+            model.addAttribute("business", business);
+            model.addAttribute("message", "프로필이 성공적으로 업데이트되었습니다.");
+        } catch (Exception e) {
+            model.addAttribute("error", "프로필 업데이트에 실패했습니다.");
+        }
 
-    @DeleteMapping("{id}")  // http://localhost:8080/api/business/id입력
-    public ResponseEntity<Void> deleteBusiness(@PathVariable("id") String businessId) {
-        businessService.deleteBusiness(businessId);
-        return ResponseEntity.noContent().build();
-    }
-    
-    @GetMapping("/login")
-	public String loginUser(@RequestParam String bId, @RequestParam String bpwd) {
-		boolean isLogin = businessService.loginBusiness(bId, bpwd);
-		
-		if (isLogin) {
-            return "로그인을 성공했습니다."; 
-        } else {
-          
-            return "아이디 또는 비밀번호가 올바르지 않습니다.";
-        }
-		
-	}
+		return "BupdateProfile";
+	}	
+	
+	/*
+	 * @PostMapping("/profile") // 사업자 프로필 수정 public String
+	 * BupdateProfile(@ModelAttribute Business updatedbusiness, HttpSession session,
+	 * Model model) { String bId = (String) session.getAttribute("bId");
+	 * 
+	 * try { Business existingBusiness = businessService.getBusinessByCode(bId);
+	 * 
+	 * existingBusiness.setBusiness_pwd(updatedbusiness.getBusiness_pwd());
+	 * 
+	 * Business business = businessService.updateBusiness(bId, existingBusiness);
+	 * 
+	 * model.addAttribute("business", business); model.addAttribute("message",
+	 * "프로필이 성공적으로 업데이트되었습니다."); } catch (Exception e) { model.addAttribute("error",
+	 * "프로필 업데이트에 실패했습니다."); }
+	 * 
+	 * return "BupdateProfile"; }
+	 */
+
 }
