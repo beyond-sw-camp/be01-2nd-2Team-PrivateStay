63aff4c67214f7bdddfad1fff7bb6ac7a9785d41
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-12 18:43:42 +0900

    2024.01.12 pm 6:43

--- PrivateStay/src/main/java/com/test/repository/CompanyRepository.java
+++ PrivateStay/src/main/java/com/test/repository/CompanyRepository.java
@@ -1,9 +1,13 @@
 package com.test.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.test.entity.Company;
 
-public interface CompanyRepository extends JpaRepository<Company, Integer>{ //Type, Key
+public interface CompanyRepository extends JpaRepository<Company, Integer>{
+
+	List<Company> findByBusiness_BusinessCode(String businessCode);
 
 }
