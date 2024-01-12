f3ce0d0e464b688b9d99b635db433a49469cf8c2
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-12 14:04:03 +0900

    2024.01.12. pm.2.00

--- PrivateStay/src/main/java/com/test/service/CompanyServiceImpl.java
+++ PrivateStay/src/main/java/com/test/service/CompanyServiceImpl.java
@@ -4,13 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.test.entity.Company;
-import com.test.service.CompanyService;
 import com.test.repository.CompanyRepository;
 
-import lombok.extern.slf4j.Slf4j;
+// import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
+// @Slf4j
 public class CompanyServiceImpl implements CompanyService{
 
 	@Autowired
@@ -18,17 +17,31 @@ public class CompanyServiceImpl implements CompanyService{
 	
 	@Override
 	public Company saveCompany(Company company) {
+		if(companyRepository.findById(company.getCompany_code()).isPresent()) {
+			throw new RuntimeException("이미 존재하는 사업장입니다.");
+		}
 		return companyRepository.save(company);
 	}
 
 	@Override
-	public Company getCompanyByCode(int companycode) {
-		return companyRepository.findById(companycode).get();
+	public Company getCompanyByCode(int Ccode) {
+		return companyRepository.findById(Ccode).get();
 	}
 	
 	@Override
-	public void deleteCompanyByCode(int companycode) {
-		companyRepository.deleteById(companycode);
+	public Company updateCompanyByCode(int Ccode, Company updatedCompany) {
+		Company exisitingCompany = companyRepository.findById(Ccode).orElse(null);
+		if (exisitingCompany != null) {
+			exisitingCompany.setCompany_name(updatedCompany.getCompany_name());
+			exisitingCompany.setCompany_addr(updatedCompany.getCompany_addr());
+			return companyRepository.save(exisitingCompany);
+		}
+		return null;
+	}
+		
+	@Override
+	public void deleteCompanyByCode(int Ccode) {
+		companyRepository.deleteById(Ccode);
 	}
 
 }
