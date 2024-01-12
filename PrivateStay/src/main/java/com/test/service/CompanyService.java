0e82044a61e103378ecc5b8cd049510da58e747a
dhkdtld37 <149128094+dhkdtld37@users.noreply.github.com>
2024-01-12 18:45:10 +0900

    2024.01.12 pm 6:43

--- PrivateStay/src/main/java/com/test/service/CompanyService.java
+++ PrivateStay/src/main/java/com/test/service/CompanyService.java
@@ -1,12 +1,15 @@
 package com.test.service;
 
+import java.util.List;
+
 import com.test.entity.Company;
 
 public interface CompanyService {
 	
 	Company saveCompany(Company company);							  // 1. 사업장 추가하기
-	Company getCompanyByCode(int Ccode);							  // 2. 사업장 조회하기
-	Company updateCompanyByCode(int Ccode, Company updatedCompany);	 			 // 3. 사업장 수정하기
-	void deleteCompanyByCode(int Ccode);							  // 4. 사업장 삭제하기 
+	List<Company> findAllCompaniesByBusinessCode(String bId);// 2. 사업장 전체 조회하기
+	Company getCompanyByCode(int Ccode);							  // 3. 사업장 선택 조회하기
+	Company updateCompanyByCode(int Ccode, Company updatedCompany);	  // 4. 사업장 수정하기
+	void deleteCompanyByCode(int Ccode);							  // 5. 사업장 삭제하기 
 	
 }
