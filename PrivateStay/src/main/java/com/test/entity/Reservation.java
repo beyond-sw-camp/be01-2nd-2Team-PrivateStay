b228f7251de299cbe032ee67bcb62ef48194ebc8
jun16017108 <150647370+jun16017108@users.noreply.github.com>
2024-01-15 12:13:47 +0900

    Update Reservation.java

--- PrivateStay/src/main/java/com/test/entity/Reservation.java
+++ PrivateStay/src/main/java/com/test/entity/Reservation.java
@@ -1,33 +1,32 @@
 package com.test.entity;
-
 import jakarta.persistence.*;
-import com.test.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 import java.time.LocalDateTime;
-import java.util.Optional;
-
+import com.test.exception.NotEnoughStockException;
 @Entity
-@Table(name = "reservation")
+@Table(name = "Reservation")
 @Getter
 @Setter
 public class Reservation {
     //******************************
     @Id
     @GeneratedValue
-    @Column(name = "reserv_id")
+    @Column(name = "reservationId")
     private Integer id;
-//********************************************
+    //********************************************
     @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "userId")
     private User user;
-//**********************************
+    //**********************************
     @Enumerated(EnumType.STRING)
     private ReservationStatus status;
-//***************************************
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    @JoinColumn(name="sCode")
+    //***************************************
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "sCode", referencedColumnName = "sCode")
     private Stock stock;
+
     //**********************************************
     private LocalDateTime currTimeStamp;
     private Integer headcount;
@@ -36,12 +35,9 @@ public class Reservation {
         this.user = user;
         user.getReservations().add(this);
     }
-    public void setStock(Stock stock) {
-        this.stock = stock;
-        stock.setReservation(this);
-    }
 
-    public static Reservation createReservation(User user, Integer headcount, String status, Stock stock) throws NotEnoughStockException {
+
+    public static Reservation createReservation(User user, Integer headcount, ReservationStatus status, Stock stock) throws NotEnoughStockException {
         boolean stockAvailability = stock.getQuantity();
         if (!stockAvailability) throw new NotEnoughStockException("no stock available");
         Reservation reservation = new Reservation();
@@ -50,19 +46,19 @@ public class Reservation {
         stock.setQuantity(false);
         reservation.setHeadcount(headcount);
         reservation.setCurrTimeStamp(LocalDateTime.now());
-
-        if (status.equals("onHold")) {
+        if (status.equals(ReservationStatus.ON_HOLD)) {
             reservation.setStatus(ReservationStatus.ON_HOLD);
-        } else if (status.equals("reserved")) {
+        } else if(status.equals(ReservationStatus.RESERVED)) {
             reservation.setStatus(ReservationStatus.RESERVED);
-        } else { // "canceled
+        } else {
             reservation.setStatus(ReservationStatus.CANCELED);
         }
         return reservation;
     }
+
     public void cancelReservation() {
         this.setStatus(ReservationStatus.CANCELED);
-        stock.setQuantity(true);
+        this.stock.setQuantity(true);
     }
 
 
