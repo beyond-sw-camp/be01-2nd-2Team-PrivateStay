63e4bef3ec9ea114d2c323bcdde2fc01838d9013
jun16017108 <150647370+jun16017108@users.noreply.github.com>
2024-01-15 12:07:39 +0900

    Update ReservationForm.java

--- PrivateStay/src/main/java/com/test/controller/ReservationForm.java
+++ PrivateStay/src/main/java/com/test/controller/ReservationForm.java
@@ -1,10 +1,14 @@
 package com.test.controller;
-
+import com.test.entity.ReservationStatus;
 import lombok.Getter;
 import lombok.Setter;
+import javax.validation.constraints.NotEmpty;
 @Getter
 @Setter
 public class ReservationForm {
-    private String userId;
-    private String sCode;
+    @NotEmpty(message = "필수입력사항입니다")
+    private ReservationStatus status;
+    @NotEmpty(message = "필수입력사항입니다.")
+    private Integer headCount;
+
 }
